2faf5e695d0123aacec4b02313e8cf46cae1590a
MaicolCode <paucarjhon07@outlook.com>
2025-03-20 23:32:44 -0500

    Refactor App component to always render MovileWindow and simplify layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,17 @@ const Tasks = lazy(() => import('@/components/sections/Tasks'))
 const RepAndRes = lazy(() => import('@/components/sections/RespAndRes'))
 
 function App() {
-  const isMobile = window.innerWidth < 480
   return (
     <>
-      {isMobile ? (
-        <MovileWindow />
-      ) : (
-        <section className='w-[480px] sm:w-[768px] h-dvh md:w-full hidden md:flex  justify-between p-10 box-border border m-[0px_auto] overflow-auto gap-2'>
-          <TaskProvider>
-            <CompleteAndIncomplete />
-            <Tasks />
-            <RepAndRes />
-          </TaskProvider>
-          <Toaster position='top-right' />
-        </section>
-      )}
+      <section className='w-[480px] sm:w-[768px] h-dvh md:w-full hidden md:flex  justify-between p-10 box-border border m-[0px_auto] overflow-auto gap-2'>
+        <TaskProvider>
+          <CompleteAndIncomplete />
+          <Tasks />
+          <RepAndRes />
+        </TaskProvider>
+        <Toaster position='top-right' />
+      </section>
+      <MovileWindow />
     </>
   )
 }
